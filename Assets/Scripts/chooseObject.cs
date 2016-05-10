5cb9911b00ca08b4aa623027679c40c26aa6b104
Clement BARBISAN <cbarbisa@e1r9p18.42.fr>
2016-05-10 16:07:11 +0200

    all features implemented not enough tests

--- Assets/Scripts/chooseObject.cs
+++ Assets/Scripts/chooseObject.cs
@@ -5,37 +5,45 @@ public class chooseObject : MonoBehaviour
 {
 	public GameObject[] list;
 	public checkSuccess[] listSuccess;
+	public int level;
+	Canvas successCanvas;
 	bool success = false;
 	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
 		list = GameObject.FindGameObjectsWithTag("model");
 		list[0].GetComponent<rotationObject>().choosen = true;
+		successCanvas = GameObject.FindGameObjectWithTag ("successCanvas").GetComponent<Canvas>();
+		successCanvas.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetMouseButtonDown(0)) 
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
+		if (success) {
+			if (PlayerPrefs.GetInt("unlock") == level)
+				PlayerPrefs.SetInt("unlock", level + 1);
+			PlayerPrefs.Save();
+			successCanvas.gameObject.SetActive(true);
+		} else {
+			if (Input.GetMouseButtonDown (0)) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
 			
-			if (Physics.Raycast(ray, out hit, 100) && hit.collider.tag == "model")
-			{
-				hit.collider.GetComponent<rotationObject>().choosen = true;
-				for (int i = 0; i < list.Length; i++)
-				{
-					if (list[i].gameObject != hit.collider.gameObject)
-						list[i].GetComponent<rotationObject>().choosen = false;
+				if (Physics.Raycast (ray, out hit, 100) && hit.collider.tag == "model") {
+					hit.collider.GetComponent<rotationObject> ().choosen = true;
+					for (int i = 0; i < list.Length; i++) {
+						if (list [i].gameObject != hit.collider.gameObject)
+							list [i].GetComponent<rotationObject> ().choosen = false;
+					}
 				}
 			}
-		}
-		for (int i = 0; i < listSuccess.Length; i++) {
-			if (listSuccess[i].success && i == listSuccess.Length - 1)
-				success = true;
-			else if (!listSuccess[i].success)
-				break;
+			for (int i = 0; i < listSuccess.Length; i++) {
+				if (listSuccess [i].success && i == listSuccess.Length - 1)
+					success = true;
+				else if (!listSuccess [i].success)
+					break;
+			}
 		}
 	}
 }
---
+++ Assets/Scripts/successButtons.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class successButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void resetSave()
+	{
+		PlayerPrefs.DeleteAll ();
+	}
+
+	public void returnToMenu()
+	{
+		Application.LoadLevel ("menu");
+	}
+
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
